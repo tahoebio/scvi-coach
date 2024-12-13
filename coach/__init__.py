8e1199d6e0c35c278af327a3149b3913beb3b5e4
Valentine Svensson <valentine@vevo.ai>
2024-12-13 19:21:12 +0000

    Made a git repo

---
+++ coach/__init__.py
@@ -0,0 +1,161 @@
+import anndata
+from scipy import sparse
+
+
+## From https://github.com/scverse/scvi-tools/issues/3018  ## 
+
+class ArrayFakerSparse(sparse.csr_matrix):
+    """Create a wrapper around a sparse matrix layer in an AnnCollection that
+    obeys the typical AnnData API"""
+
+    def __init__(self, collection, key):
+        self.collection = collection
+        self.key = key
+        # minibatch size for batchwise operations
+        self.batch_size = 256
+
+    def __getitem__(self, idx):
+        """Second layer of get item that handles returning an index
+        into a specific layer"""
+        return self.collection[idx].layers[self.key]
+
+    def getnnz(self):
+        """Run through the data in minibatches and count non-zero elements"""
+        n_batches = int(math.ceil(self.collection.shape[0] / n_batches))
+
+        nnz = 0
+
+        start = 0
+        end = self.batch_size
+        for _i in range(n_batches):
+            batch = self.collection[start:end].layers[self.key]
+            nnz += batch.getnnz()
+            start = end
+            end = min(end + self.batch_size, self.collection.shape[0])
+
+        return nnz
+
+    def _mul_vector(self, other):
+        """Included to try and trick issparse"""
+        raise NotImplementedError("This should basically never be called.")
+        return self._data.dot(other)
+
+    def _mul_multivector(self, other):
+        """Included to try and trick issparse"""
+        raise NotImplementedError("This should basically never be called.")
+        return self._data.dot(other)
+
+    def toarray(self,):
+        """Convert to a dense array.
+
+        Notes
+        -----
+        Ths would require full dense representation in memory. It shouldn't be
+        used.
+        """
+        raise NotImplementedError("This should basically never be called.")
+        return self.collection[:].layers[self.key].toarray()
+
+    def transpose(self,):
+        """Transpose the entire array.
+
+        Notes
+        -----
+        Ths would require full dense representation in memory. It shouldn't be
+        used.
+        """
+        raise NotImplementedError("This should basically never be called.")
+        return self.collection[:].layers[self.key].transpose()
+
+    def __repr__(self):
+        return f"ArrayFakerSparse(collection={self.collection}, key={self.key})"
+
+    @property
+    def data(self):
+        """Implement a reference into a subset of the non-zero values.
+
+        Notes
+        -----
+        This is used in `scvi-tools` to check that values are integers. We
+        just return a subsample for now so that existing code paths are
+        supported.
+        """
+        return self.collection[:64].layers[self.key].data
+
+    def getformat(self):
+        """Extract the relevant format (CSR, CSC, COO)"""
+        return self.collection[:4].layers[self.key].getformat()
+
+
+class LayerFaker:
+    """Custom access for .layers inside an AnnCollection that lives
+    within a CollectionAdapter"""
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    def keys(self):
+        k = self.collection[1:5].layers.keys()
+        return k
+
+    def __getitem__(self, key):
+        """First layer of get item that handles returning an index into a specific layer"""
+        # determine the array's type
+        # we first need to draw an AnnCollectionView to get an object that has
+        # access to .layers, which is not provided in AnnCollection
+        sample = self.collection[:4]
+        layer = sample.layers[key]
+
+        if isinstance(layer, sparse.spmatrix):
+            return ArrayFakerSparse(self.collection, key)
+        elif isinstance(layer, np.ndarray):
+            return ArrayFakerNDarray(self.collection, key)
+        else:
+            raise TypeError(f"Unknown array type {type(layer)}")
+
+
+class CollectionAdapter:
+    """Allow an AnnCollection to pretend to be an AnnData."""
+    # scvi-tools stores registry information here
+    uns = {}
+    # necessary for an scvi-tools check
+    isbacked = True
+    # necessary to pass an scvi-tools check that rejects views
+    is_view = False
+
+    SPECIAL_ATTRS = [
+        "uns",
+        "is_view",
+        "isbacked",
+        "layers",
+    ]
+
+    def __init__(self, collection):
+        self.collection = collection
+
+    def __getattr__(self, name):
+        if name in self.SPECIAL_ATTRS:
+            return getattr(self, name)
+        else:
+            return getattr(self.collection, name)
+
+    def __repr__(self):
+        return "Adapter for:\n" + repr(self.collection)
+
+    @property
+    def layers(self):
+        return LayerFaker(self.collection)
+
+    def __getitem__(self, idx):
+        return self.collection[idx]
+
+    def __len__(self):
+        return len(self.collection)
+
+    @property
+    def __class__(self):
+        return anndata.AnnData
+
+
+## ##
+
---
+++ scripts/train.py
@@ -0,0 +1,137 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from glob import glob
+from datetime import datetime
+
+import anndata
+from anndata.experimental.multi_files import AnnCollection
+import numpy as np
+from pytorch_lightning.loggers import CSVLogger
+from scipy import sparse
+import scvi
+from scvi.train import SaveCheckpoint
+import torch
+import tqdm
+import typer
+
+from coach import CollectionAdapter
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def main():
+    # Create an ISO timestamp
+    timestamp = datetime.now().isoformat()
+
+    base_dir = f'/vevo/drive_3/ANALYSIS/analysis_157/Data/scvi_models/{timestamp}'
+    print(f'Base directory: {base_dir}')
+
+    # Load the data
+    pattern = '/atlas/p*/*.h5ad'
+    files = glob(pattern)
+
+    adatas = []
+    for fname in files:
+        ad_ = anndata.read_h5ad(fname, backed = 'r')
+        ad_.layers['counts'] = ad_.X
+        adatas += [ad_]  
+
+    anco = AnnCollection(adatas, label = 'dataset')
+
+    print(anco)
+
+    collection_adapter = CollectionAdapter(anco)
+
+    torch.set_float32_matmul_precision('high')
+    scvi.settings.dl_num_workers = 8
+    scvi.settings.dl_persistent_workers = False
+    scvi.settings.num_threads = 8
+
+    # Set up model
+    scvi.model.SCVI.setup_anndata(
+        collection_adapter,
+        layer = 'counts',
+        size_factor_key = 'tscp_count'
+    )
+    model = scvi.model.SCVI(collection_adapter, gene_likelihood = 'nb')
+
+    # Training settings
+    batch_size = 128
+
+    # How much to log per epoch
+    val_check_interval = 0.25
+    train_log_interval = 0.05
+
+    steps_per_epoch = (collection_adapter.shape[0] // batch_size)
+    every_n_steps = int(steps_per_epoch * train_log_interval)
+
+    checkpoint_callback = SaveCheckpoint(
+        dirpath = f'{base_dir}/checkpoints/',
+        filename = 'model_epoch_{epoch}',
+        monitor = 'validation_loss'
+    )
+
+    csv_logger = CSVLogger(save_dir = base_dir + '/scvi_logs/', name = f'train')
+
+    # Run training
+    model.train(
+        enable_progress_bar = True,
+        batch_size = batch_size,
+        max_epochs = 10,
+        accelerator = 'cuda',
+        devices = [0],
+        logger = csv_logger,
+        log_every_n_steps = every_n_steps,
+        check_val_every_n_epoch = 1,
+        val_check_interval = val_check_interval,
+        callbacks = [checkpoint_callback]
+    )
+
+
+    # Generate cell representations
+    print('Training finished, creating representations...')
+    qzm, qzv = model.get_latent_representation(give_mean = False, return_dist = True)
+
+    # Manually create minified AnnData
+    all_zeros = sparse.csr_matrix(model.adata.shape)
+
+    new_adata = anndata.AnnData(X = all_zeros, obs = model.adata.obs, var = adatas[0].var)
+    new_adata.layers['counts'] = all_zeros
+
+    new_adata = model._validate_anndata(new_adata)
+
+    use_latent_qzm_key = 'X_latent_qzm'
+    use_latent_qzv_key = 'X_latent_qzv'
+
+    new_adata.obsm[use_latent_qzm_key] = qzm
+    new_adata.obsm[use_latent_qzv_key] = qzv
+
+    _ADATA_MINIFY_TYPE = scvi.data._constants.ADATA_MINIFY_TYPE.LATENT_POSTERIOR
+
+    mini_fields = model._get_fields_for_adata_minification(_ADATA_MINIFY_TYPE)
+
+    _LATENT_QZM_KEY = mini_fields[0].attr_key
+    _LATENT_QZV_KEY = mini_fields[1].attr_key
+    _OBSERVED_LIB_SIZE_KEY = mini_fields[2].attr_key
+    _ADATA_MINIFY_TYPE_UNS_KEY = mini_fields[3].attr_key
+
+    new_adata.uns[_ADATA_MINIFY_TYPE_UNS_KEY] = _ADATA_MINIFY_TYPE
+    new_adata.obsm[_LATENT_QZM_KEY] = new_adata.obsm[use_latent_qzm_key]
+    new_adata.obsm[_LATENT_QZV_KEY] = new_adata.obsm[use_latent_qzv_key]
+    new_adata.obs[_OBSERVED_LIB_SIZE_KEY] = new_adata.obs['tscp_count']
+
+    del new_adata.uns['_scvi_uuid']
+
+    model._update_adata_and_manager_post_minification(new_adata, _ADATA_MINIFY_TYPE)
+    model.module.minified_data_type = _ADATA_MINIFY_TYPE
+
+    print(model)
+    print(model.adata)
+
+    # Save model and minified AnnData
+    model.save(base_dir + '/model.minified', save_anndata = True, overwrite = True)
+
+
+if __name__ == "__main__":
+    typer.run(main)
+
